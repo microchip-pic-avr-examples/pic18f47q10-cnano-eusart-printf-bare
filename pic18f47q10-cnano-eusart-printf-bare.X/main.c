0445ada1df416657476b776de46cda51764d70b2
Alexandru Niculae <alexandru.niculae@microchip.com>
2020-04-03 13:30:38 +0300

    MPAE-1848: Added code

---
+++ pic18f47q10-cnano-eusart-printf-bare.X/main.c
@@ -0,0 +1,114 @@
+/*
+    (c) 2020 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+/* Disable Watch Dog Timer */
+#pragma config WDTE = OFF
+
+#include <xc.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#define _XTAL_FREQ 1000000UL
+#define PPS_CONFIG_RD0_EUSART2_TX 0x0B
+#define START_DATA_STREAM_PROTOCOL 0x03
+#define STOP_DATA_STREAM_PROTOCOL 0xFC
+
+static void CLK_init(void);
+static void EUSART2_init(void);
+static void PPS_init(void);
+static void PORT_init(void);
+static void EUSART2_write(uint8_t txData);
+void putch(char txData);
+
+static void CLK_init(void)
+{   
+    /* Set HFINTOSC as new oscillator source */
+//    OSCCON1 = _OSCCON1_NOSC1_MASK | _OSCCON1_NOSC2_MASK;
+    OSCCON1bits.NOSC = 0b011;
+    /* Set HFFRQ to 1 MHz */
+//    OSCFRQ = ~_OSCFREQ_HFFRQ_MASK;
+    OSCFRQbits.HFFRQ = 0;
+}
+
+static void EUSART2_init(void)
+{
+    /* Transmit Enable and High Baud Rate Select */
+    //TX2STA = _TX2STA_TXEN_MASK | _TX2STA_BRGH_MASK;
+    TX2STAbits.TXEN = 1;
+    TX2STAbits.BRGH = 1;
+    /* 16-bit Baud Rate Generator is used */
+    //BAUD2CON = _BAUD2CON_BRG16_MASK;
+    BAUD2CONbits.BRG16 = 1;
+    /*  */
+//    SP2BRGL = 0x19;
+    SP2BRGLbits.SP2BRGL = 0x19;
+    
+    /* ?Serial Port Enable */
+    //RC2STA = _RC2STA_SPEN_MASK;
+    RC2STAbits.SPEN = 1;
+}
+
+static void PPS_init(void) 
+{
+    RD0PPS = 0x0B; 
+}
+
+static void PORT_init(void)
+{
+    /* Configure RD0 as output. */
+//    TRISD &= ~(_TRISD_TRISD0_MASK);
+    TRISDbits.TRISD0 = 0;
+}
+
+static void EUSART2_write(uint8_t txData)
+{
+    while(0 == PIR3bits.TX2IF)
+    {
+        ;
+    }
+
+    TX2REG = txData; 
+}
+
+void putch(char txData)
+{
+    EUSART2_write(txData);
+}
+
+void main(void)
+{
+    uint8_t cnt = 0;
+
+    CLK_init();
+    EUSART2_init();  
+    PPS_init();   
+    PORT_init();
+    
+    while(1) 
+    {
+        printf("%c%c%c%c", START_DATA_STREAM_PROTOCOL, cnt, cnt * 2, STOP_DATA_STREAM_PROTOCOL);
+        cnt = cnt + 1;
+        __delay_ms(50);
+    }
+}
